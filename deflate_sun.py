f4723ce57a088e64748c1a7a0ca670098d1e47f8
Zachary Catlin <z@zc.is>
2020-11-24 21:03:07 -0500

    Changed scaling; bumped up iteration number

--- deflate_sun.py
+++ deflate_sun.py
@@ -9,8 +9,8 @@ def write_svg(tm, fname, include_arcs = False):
   with open(fname, 'w') as f:
     f.write('<?xml version="1.0"?>\n')
     f.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n')
-    f.write('<svg width="76mm" height="76mm" viewBox="0 0 76 76" xmlns="http://www.w3.org/2000/svg" version="1.1">\n')
-    f.write('<g transform="translate(38,38) scale(1,-1)">\n')
+    f.write('<svg width="70mm" height="70mm" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg" version="1.1">\n')
+    f.write('<g transform="translate(35,35) scale(1,-1)">\n')
     if include_arcs:
       x = to.tiling_arcs_svg(tm, 3)
       f.write('  <path style="fill: none; stroke: #f99; stroke-width: 0.5; stroke-linecap: butt;" d="{}" />\n'.format(
@@ -25,7 +25,7 @@ def write_svg(tm, fname, include_arcs = False):
     f.write('</g>\n')
     f.write('</svg>\n')
 
-init_scale = 2 * phi * phi * phi * phi * phi * phi
+init_scale = phi * phi * phi * phi * phi * phi * phi
 tm = TileManager()
 
 for i in [-1, 3, 7, 11, 15]:
@@ -33,10 +33,14 @@ for i in [-1, 3, 7, 11, 15]:
 
 tm = tm.decompose('to-A')
 
-for i in range(6):
+niter = 8
+
+for i in range(niter):
   write_svg(tm, 'sun/inter-{:02d}-A.svg'.format(i))
   write_svg(tm.decompose('to-P2'), 'sun/{:02d}-P2.svg'.format(i), True)
   tm = tm.decompose('half-deflation')
   write_svg(tm, 'sun/inter-{:02d}-B.svg'.format(i))
   write_svg(tm.decompose('to-P3'), 'sun/{:02d}-P3.svg'.format(i), True)
+  if i == niter-1:
+    break
   tm = tm.decompose('half-deflation')
